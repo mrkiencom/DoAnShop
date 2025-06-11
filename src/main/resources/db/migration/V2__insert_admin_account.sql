6e79365617fc641af1ef7126ba1a9df13c6517e0
mrkiencom <kien3031999@gmail.com>
2025-06-11 19:07:30 +0700

    .

---
+++ src/main/resources/db/migration/V2__insert_admin_account.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (id, firstname, gmail, lastname, password, picture, role, username, created_at, to_lecturer_at,
+                   is_active)
+VALUES (1,
+        'ADMIN',
+        'kien3031999@gmail.com',
+        '1',
+        '$2a$12$xiwxVj8A5//YTlH6HRfeSu51DYmB1wqLCaGA1136X9AzBlQ42XLZq',
+        '/images/default_user.jpg',
+        'admin',
+        'kien',
+        '2025-06-09 12:59:37',
+        NULL,
+        true);
